57200a585c554c8803ad37b0de5e06e9b589afdc
André Arns <andreluisarns@gmail.com>
2021-09-10 13:52:27 -0300

    Adjust CreateNewHabitVC with CustomTextField

---
+++ PersonalEvolution/views/CustomTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextField.swift
+//  PersonalEvolution
+//
+//  Created by André Arns on 09/09/21.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
