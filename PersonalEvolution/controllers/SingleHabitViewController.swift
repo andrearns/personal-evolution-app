1a6f22fff4f00d09d687c435fed80b7c7120bf1c
André Arns <andreluisarns@gmail.com>
2021-09-14 00:50:20 -0300

    Personal gallery started

--- PersonalEvolution/controllers/SingleHabitViewController.swift
+++ PersonalEvolution/controllers/SingleHabitViewController.swift
@@ -16,13 +16,29 @@ class SingleHabitViewController: UIViewController {
     @IBOutlet var habitDescriptionLabel: UILabel!
     @IBOutlet var checkinButton: UIButton!
     @IBOutlet var inviteButton: UIButton!
+    @IBOutlet var personalGaleryCollectionView: UICollectionView!
+    @IBOutlet var playPersonalRetrospectiveButton: UIButton!
+    
+    var checkins: [Checkin] = [
+        Checkin(image: UIImage(named: "galeryImageTest"), description: "descrição 1", user: nil, date: Date()),
+        Checkin(image: UIImage(named: "galeryImageTest"), description: "descrição 2", user: nil, date: Date()),
+        Checkin(image: UIImage(named: "galeryImageTest"), description: "descrição 3", user: nil, date: Date()),
+        Checkin(image: UIImage(named: "galeryImageTest"), description: "descrição 4", user: nil, date: Date()),
+        Checkin(image: UIImage(named: "galeryImageTest"), description: "descrição 5", user: nil, date: Date()),
+        Checkin(image: UIImage(named: "galeryImageTest"), description: "descrição 6", user: nil, date: Date()),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        personalGaleryCollectionView.delegate = self
+        personalGaleryCollectionView.dataSource = self
+        
         habitTitleLabel.text = habit.name
         habitDescriptionLabel.text = habit.description
         checkinButton.layer.cornerRadius = 15
         inviteButton.layer.cornerRadius = 15
+        playPersonalRetrospectiveButton.layer.cornerRadius = 15
         
         if habit.image != nil {
             habitImageView.image = CropImage.shared.crop(image: habit.image!, aspectRatio: 1.5)
@@ -61,4 +77,22 @@ class SingleHabitViewController: UIViewController {
         vc?.popUp = PopUp(image: UIImage(named: "defaultPopupImage")!, title: "Convide seus amiguinhos pra participar!", subtitle: "Evoluir em conjunto é mais legal e pode te ajudar a se motivar em momentos difíceis :)", buttonTitle: "   Compartilhar", type: .inviteFriends)
         present(vc!, animated: true)
     }
+}
+
+extension SingleHabitViewController: UICollectionViewDelegate {
+    
+}
+
+extension SingleHabitViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = personalGaleryCollectionView.dequeueReusableCell(withReuseIdentifier: "checkinCell", for: indexPath) as? GaleryCollectionViewCell
+        cell?.checkinImageView.image = checkins[indexPath.row].image
+        return cell!
+    }
+    
+    
 }
